55e472ee0c61c1199f1c254756a5c365052d23df
Pavel Belyaev <ksyddd@gmail.com>
2017-08-16 20:42:45 +0400

    ✨ Installed doctrine

--- config/bundles.php
+++ config/bundles.php
@@ -1,14 +1,9 @@
 <?php
 
 return [
-    'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => [
-        'all' => true,
-    ],
-    'Symfony\Bundle\WebServerBundle\WebServerBundle' => [
-        'dev' => true,
-    ],
-    'Symfony\Bundle\DebugBundle\DebugBundle' => [
-        'dev' => true,
-        'test' => true,
-    ],
+    'Symfony\Bundle\FrameworkBundle\FrameworkBundle' => ['all' => true],
+    'Symfony\Bundle\WebServerBundle\WebServerBundle' => ['dev' => true],
+    'Symfony\Bundle\DebugBundle\DebugBundle' => ['dev' => true, 'test' => true],
+    'Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle' => ['all' => true],
+    'Doctrine\Bundle\DoctrineBundle\DoctrineBundle' => ['all' => true],
 ];
